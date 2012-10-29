4015c5ef92612a2dca49b204002b8fd1a3367375
Kris De Volder <kdvolder@vmware.com>
2012-10-29 16:46:49 -0700

    Remove some old stuff from play-area

--- play-area/try-dnode/just-node/dnode-client.js
+++
@@ -1,9 +0,0 @@
-/*global console require*/
-var dnode = require('dnode');
-
-var d = dnode.connect(5004);
-d.on('remote', function (remote) {
-	remote.search("test", function (result) {
-		console.log(result);
-	});
-});
