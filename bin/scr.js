8b7fe2f018e8204a9ad3333268efac479b8faf21
Andy Clement <andrew.clement@gmail.com>
2013-01-15 12:30:59 -0800

    pull request for node launch scripted partially processed

--- bin/scr.js
+++ bin/scr.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-
-var path = require('path'),
-	when = require('when'),
-	optimist = require('optimist'),
-	options,
-	cmd;
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 VMware, Inc. All Rights Reserved.
+ * THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
+ * ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
+ * CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
+ * You can obtain a current copy of the Eclipse Public License from
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * Contributors:
+ * Andy Clement, Jeremy Grelle - initial version
+ ******************************************************************************/
+var optimist = require('optimist');
+var	options;
+var cmd;
 	
 options = optimist
 	.alias('h', 'help')
--- commands/restart.js
+++ commands/restart.js
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 VMware, Inc. All Rights Reserved.
+ * THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
+ * ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
+ * CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
+ * You can obtain a current copy of the Eclipse Public License from
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * Contributors:
+ * Andy Clement, Jeremy Grelle - initial version
+ ******************************************************************************/
 var kill = require('./kill'),
 	start = require('./start');
 
