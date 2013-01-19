2d295ef734ec87f6f5f30d9d8869c8991b514461
Kris De Volder <kdvolder@vmware.com>
2013-01-18 16:18:44 -0800

    Some cleanups to the trailing-whitespace sample plugin.

--- plugins/trailing-whitespace-plugin.js
+++ plugins/trailing-whitespace-plugin.js
@@ -10,24 +10,26 @@
  * Contributors:
  *     Kris De Volder (VMWare) - initial API and implementation
  ******************************************************************************/
- //Trailing white space removed by my-first-scripted-plugin
 define(function(require) {
 
 	console.log('Trailing white space removal plugin loaded');
 
 	var editorApi = require('scripted/api/editor');
-	
-	function trimLines(text) {
-		//TODO: It looks like this code below isn't quite right.
-		// Deletes blank lines of text as well.   
 
-		return text.replace(/[ \t][ \t]*$/gm, "");
-		//The nice line of code above came from here:  
+	function trimLines(text) {
+		//This nice line of code:
+		//return text.replace(/[\s][\s]*$/gm, "");
 		//http://stackoverflow.com/questions/5568797/trim-trailing-spaces-before-newlines-in-a-single-multi-line-string-in-javascript
+		//But it looks like this code below isn't quite right as
+		//it deletes blank lines of text entirely.
+
+		//Fixed version:
+		return text.replace(/[ \t][ \t]*$/gm, "");
+
 	}
 	editorApi.onSaveTransform(function (text, filePath) {
 		console.log('Trailing ws removal on: '+filePath);
-		if (/.*.js$/.test(filePath)) { //Only .js files
+		if (/.*\.js$/.test(filePath)) { //Only .js files
 			return trimLines(text);
 		}
 		//return undefined;
