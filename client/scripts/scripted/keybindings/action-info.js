15b6d4a1dd93aed4f855d3a8c9e790e08df405c5
Kris De Volder <kdvolder@vmware.com>
2013-01-31 12:53:46 -0800

    Fix problem in plugin keybinding infrastructure causing unbound actions added by plugins not to show up in the keybinder UI until new editor is opened.

--- client/scripts/scripted/keybindings/action-info.js
+++ client/scripts/scripted/keybindings/action-info.js
@@ -28,9 +28,9 @@ define(function (require) {
 
 	var deref = require('scripted/utils/deref');
 	var editorUtils = require('scripted/utils/editorUtils');
-	
+
 	var actions = {};
-	
+
 	/**
 	 * This map contains additional information that we(scripted) want to
 	 * associate with orions actions without modifying orion code.
@@ -39,7 +39,7 @@ define(function (require) {
 		//Note: orion now has a facility for attaching a readable name to an editor action
 		// when it is defined... but not all orion-defined actions actually have readable
 		// names. So here we keep our own readable names for the missing ones.
-		
+
 		"charNext": "Next Character",
 		"charPrevious": "Previous Character",
 		"deleteNext": "Delete Next Character",
@@ -83,17 +83,17 @@ define(function (require) {
 		"copy": "Copy",
 		"cut": "Cut",
 		"paste": "Paste",
-		
+
 		//New?? Orion 1.0
 		"toggleTabMode" : "Toggle Tab Mode",
 		"toggleWrapMode" : "Toggle Wrap Mode",
 		"scrollTextStart": "Scroll To Start",
 		"scrollTextEnd": "Scroll To End",
-		
+
 		"scriptedKeyHelp": "Configure Key Bindings"
 		//TODO: Most scripted specific keybindings are just using the readable description as
 		// the action ID. Maybe this should be cleaned up.
-		
+
 //		"selectAll": "Select All",
 
 
@@ -155,7 +155,7 @@ define(function (require) {
 		'moveLinesDown',
 		'gotoLine'
 	 ];
-	 
+
 	/**
 	 * Actions in this list will not
 	 * be shown in the keybinding ui.
@@ -169,13 +169,13 @@ define(function (require) {
 		'toggleTabMode', //TODO: Actually this one should not just be 'hidden' but forcibly
 						// unbound. This mode produces weird effects should user accidentally deactivate
 						// and then press 'Tab' key.
-		
+
 		//these are only active in the search popup window, can't currently rebind:
 		'findNext', 'findPrevious'
 	 ];
-	 
+
 	 //TODO: Add a list of 'invalid' (for stuff like 'toggleTabMode')
-	 
+
 	/**
 	 * Write an value at the end of a chain of properties in a nested
 	 * object structure. If the objects on the path of the chain don't
@@ -201,7 +201,7 @@ define(function (require) {
 			}
 		}
 	}
-	
+
 	function initActions() {
 		for (var id in descriptions) {
 			put(actions, [id, 'name'], descriptions[id]);
@@ -215,14 +215,14 @@ define(function (require) {
 		hiddenActions.forEach(function (id) {
 			put(actions, [id, 'category'], 'hidden');
 		});
-		
+
 		console.log('>>>> action-infos');
 		console.log(JSON.stringify(actions, null, '\t'));
 		console.log('<<<< action-infos');
 	}
-	
+
 	initActions();
-	
+
 	/**
 	 * Retrieve a 'user friendly' description for an internal action name.
 	 * This is the text that will be used to identify the action in the
@@ -261,7 +261,7 @@ define(function (require) {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Fetch 'Set' of global actions associated with an editor. The properties of the map
 	 * are all action names registered with the editor that are marked as global actions.
@@ -279,13 +279,13 @@ define(function (require) {
 
 	var handlers = {}; // All action handlers registered by plugins go in here.
 
-	function setEditorAction(editor, name, handler) {
+	function setEditorAction(editor, actionID, handler) {
 		var tv = editor.getTextView();
 		var action = function() {
 			handler(editor);
 			return true; //stop event propagation.
 		};
-		tv.setAction(name, action);
+		tv.setAction(actionID, action);
 	}
 
 	function setAction(actionID, handler, options) {
@@ -296,18 +296,20 @@ define(function (require) {
 			}
 		}
 		handlers[actionID] = handler; //TODO: should these also go in the 'integrated' actions map as properties?
-		descriptions[name] = options.description;
 		eachEditor(function (editor) {
-			setEditorAction(editor, name, handler, options);
+			setEditorAction(editor, actionID, handler, options);
 		});
+		$('#help_panel').trigger('refresh'); //TODO: correct way to do this would
+		                                     // be to publish an event that means actions have changed
+		                                     // not prod every UI element that might be interested.
 	}
 
 	$(document).on('paneCreated', function(event, pane) {
 		var editor = pane.editor;
 		if (editor) {
-			for (var name in handlers) {
-				if (handlers.hasOwnProperty(name)) {
-					setEditorAction(editor, name, handlers[name]);
+			for (var actionID in handlers) {
+				if (handlers.hasOwnProperty(actionID)) {
+					setEditorAction(editor, actionID, handlers[actionID]);
 				}
 			}
 		}
